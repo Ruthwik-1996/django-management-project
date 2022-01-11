3ee7600b76b8a709d78720c9a540c86b8280900c
Ruthwik-1996 <97428850+Ruthwik-1996@users.noreply.github.com>
2022-01-11 09:04:51 +0530

    Update wsgi.py

--- medstore/wsgi.py
+++ medstore/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medstore.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medstore.settings") # hello world
 
 application = get_wsgi_application()
