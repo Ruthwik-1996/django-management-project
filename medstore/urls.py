d89f0f0eddd782156c9dbab47ee9bad296a1b472
Ruthwik-1996 <97428850+Ruthwik-1996@users.noreply.github.com>
2022-01-11 08:52:47 +0530

    update

--- medstore/urls.py
+++ medstore/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from pharma import views
+from pharma import views # this is gowtham
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
