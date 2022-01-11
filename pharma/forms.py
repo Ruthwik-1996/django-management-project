17054971a94e34d8eb82d594effde380dd8496a5
Ruthwik-1996 <97428850+Ruthwik-1996@users.noreply.github.com>
2022-01-11 11:33:07 +0530

    Update forms.py

--- pharma/forms.py
+++ pharma/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-class UserForm(forms.ModelForm):
+class UserForm(forms.ModelForm):   # this is user form
     class Meta:
         model = User
         fields = ['cust_id', 'fname', 'lname', 'phn_no', 'address', 'med_name', 'qty']
